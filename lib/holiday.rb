adf74637caec096082ee1672b3181ff2c711c3e9
Ryan Hu <ryanhuuu@gmail.com>
2018-03-23 23:06:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,9 +95,11 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     
     holidays.each do |holiday, stuff|
+      
       if stuff.include?("BBQ")
         result << holiday
       end
+      
     end
     
   end
