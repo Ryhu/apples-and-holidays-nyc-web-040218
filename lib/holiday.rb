3da85790a4a92d953844c797606798a0fcb92f2c
Ryan Hu <ryanhuuu@gmail.com>
2018-03-23 22:57:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holidays.each do |holidayName, supply|
       squish = supply.join(", ")
-      holidayName.to_s.split("_").map(:&capitalize).join
+      holidayName.to_s.split("_").map(&:capitalize).join(" ")
       
       puts "  #{}: #{squish}"
     end
